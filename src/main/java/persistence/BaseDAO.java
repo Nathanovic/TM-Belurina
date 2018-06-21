a898f6f9495746de70c5a5a402e7acb4f7b63677
lb321 <lucas.001@live.nl>
2018-06-21 22:36:32 +0200

    testuser

--- src/main/java/persistence/BaseDAO.java
+++ src/main/java/persistence/BaseDAO.java
@@ -7,6 +7,6 @@ import java.sql.SQLException;
 public abstract class BaseDAO {
 
     protected Connection getConnection() throws SQLException, ClassNotFoundException {
-        return DriverManager.getConnection("jdbc:mysql://localhost:3306/test?autoReconnect=true&useSSL=false", "root", "Database12#");
+        return DriverManager.getConnection("jdbc:mysql://localhost:3306/test?autoReconnect=true&useSSL=false", "test", "Testing@12");
     }
 }
