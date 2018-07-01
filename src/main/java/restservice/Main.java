b1aa4f841febe4c521fe243c69dc28ee492f9d7f
lb321 <lucas.001@live.nl>
2018-07-01 20:26:09 +0200

    allow cors request

--- src/main/java/restservice/Main.java
+++ src/main/java/restservice/Main.java
@@ -14,7 +14,9 @@ import restservice.controller.TourController;
 import restservice.controller.UserController;
 import restservice.exception.badrequest.BadRequestException;
 
+import static spark.Spark.before;
 import static spark.Spark.exception;
+import static spark.Spark.options;
 
 public class Main {
 
@@ -27,6 +29,25 @@ public class Main {
         TeamManager teamManager = app.getOrThrow(TeamManager.class);
         UserManager userManager = app.getOrThrow(UserManager.class);
         UserTeamManager userTeamManager = app.getOrThrow(UserTeamManager.class);
+
+        options("/*",(request, response) -> {
+            String accessControlRequestHeaders = request
+                    .headers("Access-Control-Request-Headers");
+            if (accessControlRequestHeaders != null) {
+                response.header("Access-Control-Allow-Headers",
+                        accessControlRequestHeaders);
+            }
+
+            String accessControlRequestMethod = request
+                    .headers("Access-Control-Request-Method");
+            if (accessControlRequestMethod != null) {
+                response.header("Access-Control-Allow-Methods",
+                        accessControlRequestMethod);
+            }
+
+            return "OK";
+        });
+        before(((request, response) -> response.header("Access-Control-Allow-Origin", "*")));
 
         TourController tourController = new TourController(tourManager,teamManager, "/tours");
         TeamController teamController = new TeamController(teamManager, runnerManager, "/teams");
